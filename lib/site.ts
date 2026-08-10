35fba8a98d63eb04ec97d5b841afa06313abfb77
ruoyang2026 <13249659@qq.com>
2026-08-11 00:03:43 +0800

    config: set Basin form endpoint for free sample lead capture

--- lib/site.ts
+++ lib/site.ts
@@ -2,4 +2,4 @@ export const SITE_URL = "https://www.deep-time-studio.com";
 export const SITE_NAME = "Deep Time Studio";
 export const SHOP_URL = "https://chenyang84.gumroad.com/";
 export const FREE_SAMPLE_URL = "/free-trilobite-sample.pdf";
-export const BASIN_ENDPOINT = "https://usebasin.com/f/YOUR_FORM_ID";
+export const BASIN_ENDPOINT = "https://usebasin.com/f/a78f43b5fbd7";
